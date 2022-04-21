6e694f115939c161a4d150599c8a53f6fe452cd5
Agyr0 <103292637+Agyr0@users.noreply.github.com>
2022-04-21 11:54:20 -0700

    Lava Floor

---
+++ Assets/LavaFloor.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LavaFloor : MonoBehaviour
+{
+    public Switch[] switches;
+
+    public GameObject point1;
+    public GameObject point2;
+    public GameObject point3;
+    public GameObject point4;
+
+
+    private Vector3 pos1;
+    private Vector3 pos2;
+    private Vector3 pos3;
+    private Vector3 pos4;
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pos1 = point1.transform.position;
+        pos2 = point2.transform.position;
+        pos3 = point3.transform.position;
+        pos4 = point4.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SwitchCheck();
+    }
+
+    public void SwitchCheck()
+    {
+        foreach (Switch sw in switches)
+        {
+            if (sw.isOn)
+            {
+                MoveLava();
+                Debug.Log("increased lava height");
+
+            }
+        }
+    }
+
+    public void MoveLava()
+    {
+        if(transform.position == pos1)
+        {
+            transform.Translate(0, 10, 0);
+        }
+    }
+
+}
