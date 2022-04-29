6a7018f55065d4df74f4ee4f248df599f633a118
Agyr0 <tgholmes@mail.csuchico.edu>
2022-04-29 15:02:46 -0700

    boss room stuff

---
+++ Assets/ProjectileKiller.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileKiller : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Lava")
+        {
+            Destroy(other);
+        }
+        if (other.gameObject.tag == "Water")
+        {
+            Destroy(other);
+        }
+    }
+}
