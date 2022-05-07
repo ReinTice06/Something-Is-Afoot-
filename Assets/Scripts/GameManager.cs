4ac672aea6bab9990eba0350d5d935765b7bc88f
Agyr0 <tgholmes@mail.csuchico.edu>
2022-05-06 20:06:58 -0700

    stuff and things

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -20,9 +20,9 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        DontDestroyOnLoad(Canvas);
+        //DontDestroyOnLoad(Canvas);
         DontDestroyOnLoad(Counter);
-        DontDestroyOnLoad(Player);
+        //DontDestroyOnLoad(Player);
         DontDestroyOnLoad(this.gameObject);
         
     }
